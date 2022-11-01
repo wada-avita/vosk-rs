82e2a197c4813f65ce58dbd72c2de55629c5e852
wada <wada@avita.co.jp>
2022-11-01 15:17:50 +0900

    print summary

--- vosk/examples/microphone.rs
+++ vosk/examples/microphone.rs
@@ -55,21 +55,50 @@ fn main() {
         eprintln!("an error occurred on stream: {}", err);
     };
 
+    let current_text = Arc::new(Mutex::new(String::new()));
+    let prev_partial = Arc::new(Mutex::new(String::new()));
+
     let recognizer_clone = recognizer.clone();
+    let current_text_clone = current_text.clone();
+    let prev_partial_clone = prev_partial.clone();
     let stream = match config.sample_format() {
         SampleFormat::F32 => audio_input_device.build_input_stream(
             &config.into(),
-            move |data: &[f32], _| recognize(&mut recognizer_clone.lock().unwrap(), data, channels),
+            move |data: &[f32], _| {
+                recognize(
+                    &mut recognizer_clone.lock().unwrap(),
+                    data,
+                    channels,
+                    &mut current_text_clone.lock().unwrap(),
+                    &mut prev_partial_clone.lock().unwrap(),
+                )
+            },
             err_fn,
         ),
         SampleFormat::U16 => audio_input_device.build_input_stream(
             &config.into(),
-            move |data: &[u16], _| recognize(&mut recognizer_clone.lock().unwrap(), data, channels),
+            move |data: &[u16], _| {
+                recognize(
+                    &mut recognizer_clone.lock().unwrap(),
+                    data,
+                    channels,
+                    &mut current_text_clone.lock().unwrap(),
+                    &mut prev_partial_clone.lock().unwrap(),
+                )
+            },
             err_fn,
         ),
         SampleFormat::I16 => audio_input_device.build_input_stream(
             &config.into(),
-            move |data: &[i16], _| recognize(&mut recognizer_clone.lock().unwrap(), data, channels),
+            move |data: &[i16], _| {
+                recognize(
+                    &mut recognizer_clone.lock().unwrap(),
+                    data,
+                    channels,
+                    &mut current_text_clone.lock().unwrap(),
+                    &mut prev_partial_clone.lock().unwrap(),
+                )
+            },
             err_fn,
         ),
     }
@@ -88,6 +117,8 @@ fn recognize<T: Sample + ToSample<i16>>(
     recognizer: &mut Recognizer,
     data: &[T],
     channels: ChannelCount,
+    current_text: &mut String,
+    prev_partial: &mut String,
 ) {
     let data: Vec<i16> = data.iter().map(|v| v.to_sample()).collect();
     let data = if channels != 1 {
@@ -101,8 +132,9 @@ fn recognize<T: Sample + ToSample<i16>>(
         DecodingState::Running => {
             // println!("partial: {:#?}", recognizer.partial_result());
             let partial_result = recognizer.partial_result().partial;
-            if !partial_result.is_empty() {
-                println!("partial: {}", partial_result);
+            if partial_result != prev_partial {
+                *prev_partial = partial_result.to_string();
+                println!("partial: {}", *prev_partial);
             }
         }
         DecodingState::Finalized => {
@@ -110,7 +142,11 @@ fn recognize<T: Sample + ToSample<i16>>(
             // println!("result: {:#?}", recognizer.result().multiple().unwrap());
             let final_result_multiple = recognizer.result().multiple().unwrap();
             match final_result_multiple.alternatives.iter().next() {
-                Some(alternative) => println!("final: {}", alternative.text),
+                Some(alternative) => {
+                    *current_text =
+                        (*current_text).as_str().to_string() + &alternative.text.to_string();
+                    println!("final: {}", current_text);
+                }
                 _ => (),
             };
         }
